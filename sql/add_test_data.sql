3aa930eb953f8b92221b963c863403c809cd6dd4
Heikki Leinonen <heileino@cs.helsinki.fi>
2015-11-08 23:16:07 +0200

    sql-päivitys

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -3,5 +3,9 @@
 INSERT INTO Jarjestaja (nimi, tunnus, salasana) VALUES ('Kampin Hiihtoseura', 'kamppi', 'kamppi123');
 INSERT INTO Jarjestaja (nimi, tunnus, salasana) VALUES ('Töölön Sauva', 'töölö', 'töölö123');
 -- Kilpailu-taulun testidata
-INSERT INTO Kilpailu (nimi, paivamaara, alkamisaika, jarjestaja) VALUES ('Kampin kisat miehet 30km vapaa', '2015-12-26', '12:00', (SELECT id from Jarjestaja WHERE nimi='Kampin Hiihtoseura'));
-INSERT INTO Kilpailu (nimi, paivamaara, alkamisaika, jarjestaja) VALUES ('Töölön hiihtokarnevaalit miehet 15km perinteinen', '2015-12-27', '13:00', (SELECT id from Jarjestaja WHERE nimi='Töölön Sauva'));
+INSERT INTO Kilpailu (nimi, paivamaara, alkamisaika) VALUES ('Kampin kisat miehet 30km vapaa', '2015-12-26', '12:00');
+INSERT INTO Kilpailu (nimi, paivamaara, alkamisaika) VALUES ('Töölön hiihtokarnevaalit miehet 15km perinteinen', '2015-12-27', '13:00');
+--Kilpailija-taulun testidata
+INSERT INTO Kilpailija (nimi, seura, kansallisuus, syntymavuosi) VALUES ('Hirmu Hiihtäjä', 'Kampin Hiihtoseura', 'FIN', 1949);
+--Kirjaaja-taulun testidata
+INSERT INTO Kirjaaja (nimi, tunnus, salasana) VALUES ('Kimmo Kirjuri', 'kimmo', 'kimmo123');
